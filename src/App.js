e84438af2abeff460e46b56699187b977c2384d8
Rahul Khandelwal <mrkhandelwal19@gmail.com>
2022-07-01 00:52:57 +0530

    add: navbar & sidebar

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./App.css";
+import { Home } from "./pages/home/home";
 
 function App() {
   return (
     <>
-      <h1>Namaste Rahul</h1>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" exact element={<Home />} />
+        </Routes>
+      </BrowserRouter>
     </>
   );
 }
